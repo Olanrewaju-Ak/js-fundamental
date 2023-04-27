3cffac411a10da389fb6d738d02882d262de24e7
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-04-26 21:18:55 -0700

    hundreds task done

---
+++ hundreds.js
@@ -0,0 +1,6 @@
+function howManyHundreds(num) {
+  const division = num / 100;
+  return Math.floor(division);
+}
+
+console.log(howManyHundreds(99));
--- snapshot.js
+++ snapshot.js
@@ -1,27 +1,27 @@
-// const moves = ["north", "north", "west", "west", "north", "east", "north", "west"];
-// // loop theough the moves and sort the moves
-// // north evaluates to +1, south to -1, west to -1 and east to  +1
-// //split the moves into two arrays vertical and horizontal and reduce each.
-// //final move = [horizontal, vertical]
-// function finalPosition(array) {
-//   let currentPosition = [0, 0];
+const moves = ["north", "north", "west", "west", "north", "east", "north", "west"];
+// loop theough the moves and sort the moves
+// north evaluates to +1, south to -1, west to -1 and east to  +1
+//split the moves into two arrays vertical and horizontal and reduce each.
+//final move = [horizontal, vertical]
+function finalPosition(array) {
+  let currentPosition = [0, 0];
 
-//   for (const move of array) {
-//     if (move === "north") {
-//       currentPosition[1]++;
-//     } else if (move === "south") {
-//       currentPosition[1]--;
-//     } else if (move === "east") {
-//       currentPosition[0]++;
-//     } else {
-//       currentPosition[0]--;
-//     }
-//   }
-//   console.log(currentPosition);
-//   return currentPosition;
-// }
+  for (const move of array) {
+    if (move === "north") {
+      currentPosition[1]++;
+    } else if (move === "south") {
+      currentPosition[1]--;
+    } else if (move === "east") {
+      currentPosition[0]++;
+    } else {
+      currentPosition[0]--;
+    }
+  }
+  console.log(currentPosition);
+  return currentPosition;
+}
 
-// finalPosition(moves);
+finalPosition(moves);
 
 function ageCalculator(name, yearOfBirth, currentYear) {
   const age = currentYear - yearOfBirth;
